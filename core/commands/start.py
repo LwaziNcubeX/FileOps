5d275c9f407ef48b6f6209df3a9fb8468b859854
Lwazi Ncube <lwazincubex@gmail.com>
2024-05-19 12:43:54 +0200

    docs: update function description

--- core/commands/start.py
+++ core/commands/start.py
@@ -8,8 +8,12 @@ from telegram.ext import CommandHandler, ContextTypes
 
 async def start_command(update: Update,
                         context: ContextTypes.DEFAULT_TYPE) -> None:
-    """Starts the bot"""
-    await update.message.reply_text("Hello! I'm FileOps Bot")
+    """
+    Sends a greeting message to the user when the '/start' command is issued.
+    """
+    await update.message.reply_text(
+        f"Hello! {update.effective_user.first_name}, "
+        f"I'm FileOps Bot")
 
 
 start_handler = CommandHandler("start", start_command)
