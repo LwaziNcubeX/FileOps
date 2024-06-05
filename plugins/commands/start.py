c5d43f36eac40c093c9e6907a39dc8b1b9e9be22
Lwazi Ncube <lwazincubex@gmail.com>
2024-06-05 10:29:03 +0200

    Update start command to fetch messages from translations

--- plugins/commands/start.py
+++ plugins/commands/start.py
@@ -2,18 +2,24 @@
 """
 start command
 """
-from telegram import Update
+from telegram import Update, constants
 from telegram.ext import CommandHandler, ContextTypes
+from database.db import DATABASE
+from plugins.helpers.escape_markdown import escape_markdown
+from translations.lang import get_translation
 
 
-async def start_command(update: Update,
-                        context: ContextTypes.DEFAULT_TYPE) -> None:
+async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """
     Sends a greeting message to the user when the '/start' command is issued.
     """
-    await update.message.reply_text(
-        f"Hello! {update.effective_user.first_name}, "
-        f"I'm FileOps Bot")
+    await DATABASE.check_document_id(update)
 
+    start_message = await get_translation("start_message", "Welcome!", update)
+
+    first_name = update.effective_user.first_name
+    start_message = start_message.format(name=first_name)
+
+    await update.message.reply_text(start_message, parse_mode=constants.ParseMode.HTML, quote=True)
 
 start_handler = CommandHandler("start", start_command)
